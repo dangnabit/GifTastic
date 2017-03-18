67d37328e383fa0cc2b115630f8ba638b3254fb9
dangnabit <dan.gabel@gmail.com>
2017-03-18 09:51:06 -0500

    Changed starting reaction, updated verbage in html

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
 
         $(this).toggleClass('btn-info');
 
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + reactionLbl + "&api_key=dc6zaTOxFJmzC&limit=10&offset=" + offsetRand;
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + reactionLbl + "&api_key=dc6zaTOxFJmzC&limit=10&offset=" + offsetRand;
 
         console.log(queryURL);
 
@@ -159,5 +159,5 @@ $(document).ready(function() {
 
 
     renderButtons();
-    renderGifs('Fun');
+    renderGifs('Laughing');
 });
